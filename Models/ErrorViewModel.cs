0a8915173a0af1577965d181d79358bfe5b97a9f
Dana22200 <dana.0320@mail.ru>
2021-10-25 16:36:09 +0600

    test

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dani.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
